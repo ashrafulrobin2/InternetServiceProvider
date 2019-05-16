21844486a686c010155d995d5d9834a327f09b6c
Ashraful Robin <robinashraful71@gmail.com>
2019-05-16 16:39:13 +0600

    Design Invoice List and Ticket List

---
+++ app/src/main/java/com/eomsbd/isp/activity/Invoice_List_Activity.java
@@ -0,0 +1,15 @@
+package com.eomsbd.isp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eomsbd.isp.R;
+
+public class Invoice_List_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invoice__list_);
+    }
+}
